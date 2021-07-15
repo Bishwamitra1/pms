7244f0eb8a63eef1ad8ea9bfdf31b39a3c62f7fd
minh-tran6 <minh.tran6@mavs.uta.edu>
2021-07-15 12:58:56 -0500

    health button (inital UI)

--- app/src/main/java/com/CSE3311/personalhealthmanagementsystem/navbarui/HealthFragment.java
+++ app/src/main/java/com/CSE3311/personalhealthmanagementsystem/navbarui/HealthFragment.java
@@ -19,7 +19,7 @@ public class HealthFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_health, container, false);
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
         return v;
     }
 }
