659a4fee00bbcf13444f770e0fce7c8922f5f133
minh-tran6 <minh.tran6@mavs.uta.edu>
2021-07-15 13:38:15 -0500

    health button (inital UI)

--- app/src/main/java/com/CSE3311/personalhealthmanagementsystem/navbarui/HealthFragment.java
+++ app/src/main/java/com/CSE3311/personalhealthmanagementsystem/navbarui/HealthFragment.java
@@ -4,31 +4,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
-import com.CSE3311.personalhealthmanagementsystem.HomePageActivity;
 import com.CSE3311.personalhealthmanagementsystem.R;
-
-import static com.CSE3311.personalhealthmanagementsystem.HomePageActivity.UserID;
-import static com.CSE3311.personalhealthmanagementsystem.HomePageActivity.localDB;
 
 public class HealthFragment extends Fragment {
 
     public HealthFragment() {
         // Required empty public constructor
     }
-    private TextView txtv;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_health, container, false);
+        View v = inflater.inflate(R.layout.fragment_heath, container, false);
 
-        txtv = v.findViewById(R.id.healthText);
-        String toText = String.valueOf(UserID);
-        txtv.setText("The current user's id is " + toText +"\nThe current users username is " + localDB.daointerface().getUserById(UserID).getUsername());
         return v;
     }
 }
--- app/src/main/java/com/CSE3311/personalhealthmanagementsystem/navbarui/HomeFragment.java
+++ app/src/main/java/com/CSE3311/personalhealthmanagementsystem/navbarui/HomeFragment.java
@@ -4,10 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 import com.CSE3311.personalhealthmanagementsystem.R;
+
+import static com.CSE3311.personalhealthmanagementsystem.HomePageActivity.UserID;
+import static com.CSE3311.personalhealthmanagementsystem.HomePageActivity.localDB;
 
 public class HomeFragment extends Fragment {
 
@@ -15,12 +19,17 @@ public class HomeFragment extends Fragment {
     public HomeFragment() {
         // Required empty public constructor
     }
+    private TextView txtv;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_health, container, false);
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+
+        txtv = v.findViewById(R.id.healthText);
+        String toText = String.valueOf(UserID);
+        txtv.setText("The current user's id is " + toText +"\nThe current users username is " + localDB.daointerface().getUserById(UserID).getUsername());
         return v;
     }
 }
