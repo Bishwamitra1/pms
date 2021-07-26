627072356d6d0d29d7bcdefc3ff11740d10eeb7b
minh-tran6 <minh.tran6@mavs.uta.edu>
2021-07-26 14:10:58 -0500

    addMed

--- app/src/main/java/com/CSE3311/personalhealthmanagementsystem/navbarui/medication/AddMedFragment.java
+++ app/src/main/java/com/CSE3311/personalhealthmanagementsystem/navbarui/medication/AddMedFragment.java
@@ -1,13 +1,22 @@
 package com.CSE3311.personalhealthmanagementsystem.navbarui.medication;
 
+import android.app.TimePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.fragment.app.Fragment;
 
 import com.CSE3311.personalhealthmanagementsystem.R;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class AddMedFragment extends Fragment {
 
@@ -16,11 +25,49 @@ public class AddMedFragment extends Fragment {
         // Required empty public constructor
     }
 
+    TextView inputStartTime;
+    int hour, minute;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_add_med, container, false);
+        inputStartTime = v.findViewById(R.id.inputStartTime);
+
+        inputStartTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                TimePickerDialog timePickerDialog = new TimePickerDialog(
+                        getContext(), android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                                hour = i;
+                                minute = i1;
+                                String time = i+":"+i1;
+                                SimpleDateFormat f24Hours = new SimpleDateFormat(
+                                        "HH:mm"
+                                );
+                                try {
+                                    Date date = f24Hours.parse(time);
+                                    SimpleDateFormat f12Hours = new SimpleDateFormat(
+                                            "hh:mm aa"
+                                    );
+                                    inputStartTime.setText(f12Hours.format(date));
+                                } catch (ParseException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        },12,0,false
+                );
+                timePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                timePickerDialog.updateTime(hour,minute);
+                timePickerDialog.show();
+            }
+        });
+
+
         return v;
     }
 }
