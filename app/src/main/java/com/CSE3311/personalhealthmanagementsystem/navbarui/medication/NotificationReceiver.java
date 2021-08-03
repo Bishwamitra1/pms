45ca8675ec21abc8dde61e7e50b6111daad51418
minh-tran6 <minh.tran6@mavs.uta.edu>
2021-08-03 00:16:57 -0500

    Notification

--- app/src/main/java/com/CSE3311/personalhealthmanagementsystem/navbarui/medication/NotificationReceiver.java
+++ app/src/main/java/com/CSE3311/personalhealthmanagementsystem/navbarui/medication/NotificationReceiver.java
@@ -6,18 +6,14 @@ import android.content.Intent;
 import android.widget.Toast;
 
 public class NotificationReceiver extends BroadcastReceiver {
-    public static final String Channel_ID = "channel_id";
     @Override
     public void onReceive(Context context, Intent intent) {
         Toast.makeText(context,"Alarm recieved",Toast.LENGTH_SHORT).show();
 
-//        NotificationChannel notificationChannel = new NotificationChannel(Channel_ID,"channel_id", NotificationManager.IMPORTANCE_HIGH);
-//        Notification notification = new NotificationCompat.Builder(getApplicationContext(),Channel_ID)
-//                .setContentTitle("hello")
-//                .setContentText("hello1")
-//                .setPriority(NotificationCompat.PRIORITY_HIGH)
-//                .build();
+
+
 
     }
+
 }
 
